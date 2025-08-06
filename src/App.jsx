5532d91c139b90d434fabdc4b3b48c7020db0891
himanshu-bro <himanshushekhar9899@gmail.com>
2025-08-07 03:43:13 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useGSAP } from '@gsap/react'
 gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies 
 import { gsap } from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './Components/Navbar';
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-8xl text-indigo-50 flex-center h-[100vh]'>Learn GSAP very Easy</h1>
-    </div>
+    <main>
+      <Navbar/>
+      <h1 className='flex-center h-[100vh]'>Gsap Course</h1>
+    </main>
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-amber-100 h-[100vh]'>Navbar</div>
+  )
+}
+
+export default Navbar
